f57aae5e4dd5d0200f855a93a6cb0287c8027418
Nguyen Trung Tin <nguyentrungtin.tech@gmail.com>
2022-07-16 11:01:10 +0700

    Tin - UI home page 1st section

--- components/Layouts/Navbar.jsx
+++ components/Layouts/Navbar.jsx
@@ -14,8 +14,19 @@ const Navbar = () => {
         styles.hamberger,
         hambergerToggle ? styles.active : ''
     );
+
+    const hambergerTagStyles = classnames(
+        styles.navItemMobile,
+        hambergerToggle ? styles.open : ''
+    );
+    const isActive = classnames(hambergerToggle ? styles.fade : '');
+
+    const isNavActive = classnames(
+        styles.nav,
+        hambergerToggle ? styles.active : ''
+    );
     return (
-        <div className={styles.nav}>
+        <div className={isNavActive}>
             <ul className={styles.navItem}>
                 <li className={styles.tag}>Dịch Vụ</li>
                 <li className={styles.tag}>Sản Phẩm</li>
@@ -35,6 +46,26 @@ const Navbar = () => {
                     <span></span>
                     <span></span>
                     <span></span>
+                </li>
+            </ul>
+            <ul className={hambergerTagStyles}>
+                <li className={isActive}>
+                    <a href="#">Dịch Vụ</a>
+                </li>
+                <li className={isActive}>
+                    <a href="#">Sản Phẩm</a>
+                </li>
+                <li className={isActive}>
+                    <a href="#">Khóa Học</a>
+                </li>
+                <li className={isActive}>
+                    <a href="#">Khuyến Mãi</a>
+                </li>
+                <li className={isActive}>
+                    <a href="#">Tin Tức</a>
+                </li>
+                <li className={isActive}>
+                    <a href="#">Chúng Tôi</a>
                 </li>
             </ul>
         </div>
