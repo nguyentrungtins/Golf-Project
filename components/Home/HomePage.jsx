69de865897daadb17d0da9969bc1d4636761f6fe
Nguyen Trung Tin <nguyentrungtin.tech@gmail.com>
2022-07-16 09:58:06 +0700

    Tin- add responsive for home page 1st section

--- components/Home/HomePage.jsx
+++ components/Home/HomePage.jsx
@@ -1,9 +1,21 @@
 import styles from './HomePage.module.scss';
 import Navbar from '../Layouts/Navbar';
+import scrolldown from '../../public/scrolldown.png';
+import Image from 'next/image';
 const HomePage = () => {
     return (
         <div className={styles.bg}>
             <Navbar></Navbar>
+            <div className={styles.content}>
+                <h1>World Class Golf Instruction</h1>
+                <button>EXPLORE NOW</button>
+            </div>
+            <div className={styles.scrollDown}>
+                <p>Scroll Down</p>
+                <div>
+                    <Image src={scrolldown} alt="Logo Image" />
+                </div>
+            </div>
         </div>
     );
 };
