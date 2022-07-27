0515d580b1a223f21d2837d83a434eb74f220334
Nguyen Trung Tin <nguyentrungtin.tech@gmail.com>
2022-07-27 17:16:45 +0700

    Tin - Add Loading Spinner

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,44 @@ import '../styles/globals.scss';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../styles/custom-slick-slider.scss';
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+function Loading() {
+    const router = useRouter();
 
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const handleStart = (url) => url !== router.asPath && setLoading(true);
+        const handleComplete = (url) =>
+            url === router.asPath && setLoading(false);
+
+        router.events.on('routeChangeStart', handleStart);
+        router.events.on('routeChangeComplete', handleComplete);
+        router.events.on('routeChangeError', handleComplete);
+
+        return () => {
+            router.events.off('routeChangeStart', handleStart);
+            router.events.off('routeChangeComplete', handleComplete);
+            router.events.off('routeChangeError', handleComplete);
+        };
+    });
+
+    return (
+        loading && (
+            <div className="spinner-wrapper">
+                <div className="spinner"></div>
+            </div>
+        )
+    );
+}
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Loading />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
