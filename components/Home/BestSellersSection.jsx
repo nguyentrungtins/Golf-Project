caa363bab7995951e8a8dcd7eaa5565c721afe40
Nguyen Trung Tin <nguyentrungtin.tech@gmail.com>
2022-09-01 10:03:44 +0700

    Fix UI

--- components/Home/BestSellersSection.jsx
+++ components/Home/BestSellersSection.jsx
@@ -4,7 +4,7 @@ import styles from './BestSellersSection.module.scss';
 import Button from '../Button';
 import ProductsSliderSection from '../Product/ProductsSliderSection';
 
-const BestSellers = ({ discountProducts = [] }) => {
+const BestSellers = () => {
     return (
         <div className={styles.container}>
             {/* HEADER */}
@@ -30,7 +30,7 @@ const BestSellers = ({ discountProducts = [] }) => {
             </div>
 
             {/* PRODUCTS */}
-            <ProductsSliderSection products={discountProducts} />
+            <ProductsSliderSection />
 
             {/* BUTTON */}
             <div className={styles.btnShopNow}>
